2bdbaad73e9b7917d09ffc3f1791ff77273ad379
AbinayaArul <abinaya.palani@a-cti.com>
2020-06-22 17:22:07 +0530

    updates

---
+++ test/examples.test.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+describe('First test suite', ()=>{
+
+    it('launch the browser and load the url', async() =>{
+        const browser = await puppeteer.launch(
+            {
+            headless: false
+        }); 
+
+        const page = await browser.newPage();
+        const url = 'https://example.com';
+        await page.goto(url);
+
+        await browser.close();
+    })
+
+})
+
+
+// (async () => {
+//     const browser = await puppeteer.launch({headless: true}); 
+//     const page = await browser.newPage();
+//     await page.goto('https://example.com');
+//     await page.screenshot({path: 'example.png'});
+  
+//     await browser.close();
+// })()
