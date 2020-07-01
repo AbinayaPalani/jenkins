7c0c2e8e855f6e46dd99fdc26736b1981880d434
AbinayaArul <abinaya.palani@a-cti.com>
2020-07-01 17:49:00 +0530

    resolve in time out for jenkins

--- tests/Login.test.js
+++ tests/Login.test.js
@@ -13,22 +13,24 @@ it('Admin tool ', async () => {
         args:['--start-maximized']
     });
         page = await browser.newPage();
-        await page.goto(URL, {waitUntil: ["load", "networkidle2"]});
+        const navigationPromise = page.waitForNavigation({waitUntil: "domcontentloaded"});
         
+        await page.goto(URL, {waitUntil: ["load", "networkidle2"]});
+        await navigationPromise;
         const title = await page.title();
         expect(title).toBe('AnswerConnect');
         console.log("Title of CWA "+title);
         await page.type('input[name="email"]','abinaya.palani@anywhere.co');
         await page.type('input[name="password"]', 'mani1798');
         await page.click('button.button-primary ');
-
+        await navigationPromise;
         await page.waitFor(15000);
-        await page.waitFor('#fetchAccountInput');
+        await page.waitForSelector('#fetchAccountInput');
         await page.type('#fetchAccountInput','9223300271');
         await page.click('a#fetch_ac_btn.fetch_ac_btn.search');
         await page.waitFor(5000);
         await page.click('a#fetch_ac_btn.fetch_ac_btn.search');
-
+        await navigationPromise;
         await page.waitFor(15000);
         await page.click('li#billing');
         await page.waitFor(2000);
