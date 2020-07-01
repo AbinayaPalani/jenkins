8ab8d9a2a2665522e597ec7815760b014f182943
AbinayaArul <abinaya.palani@a-cti.com>
2020-07-01 17:26:53 +0530

    admin tool for jenkins

--- tests/Login.test.js
+++ tests/Login.test.js
@@ -5,64 +5,41 @@ jest.setTimeout(50000);
 const URL = 'https://staging.access.answerconnect.com';
 
 let browser, page;
-
-beforeAll(async () => {
+describe('Suite for admin tool on jenkins',()=>{
+it('Admin tool ', async () => {
      browser = await puppeteer.launch({
         headless : truet,
         defaultViewport: null,
         devtools: false,
         args:['--start-maximized']
-    })
-     page = await browser.newPage();
-    await page.goto(URL, {waitUntil: ["load", "networkidle2"]});
-
-});
-
-describe('Admin tool feature', () => {
-
-
-    it('Title of the page', async () => {
+    });
+        page = await browser.newPage();
+        await page.goto(URL, {waitUntil: ["load", "networkidle2"]});
         
         const title = await page.title();
         expect(title).toBe('AnswerConnect');
         console.log("Title of CWA "+title)
-        
-    }, timeout);
-
-
-
-    it('Test login into the cwa page', async () =>{
         await page.type('input[name="email"]','abinaya.palani@anywhere.co');
         await page.type('input[name="password"]', 'mani1798');
         await page.click('button.button-primary ');
-    });
 
+        await page.waitFor(10000);
+        await page.waitFor('#fetchAccountInput');
+        await page.type('#fetchAccountInput','9223300271');
+        await page.click('a#fetch_ac_btn.fetch_ac_btn.search');
+        await page.waitFor(5000);
+        await page.click('a#fetch_ac_btn.fetch_ac_btn.search');
 
-
-
-    
-        it('Place the account in search box ', async () => {
-            await page.waitFor(10000);
-            await page.waitFor('#fetchAccountInput');
-            await page.type('#fetchAccountInput','9223300271');
-            await page.click('a#fetch_ac_btn.fetch_ac_btn.search');
-            await page.waitFor(5000);
-            await page.click('a#fetch_ac_btn.fetch_ac_btn.search');
-        });
-    
-        it('Enter into billing page', async () =>{
             
-            await page.waitFor(10000);
-            await page.click('li#billing');
-            await page.waitFor(2000);
-        });
+        await page.waitFor(10000);
+        await page.click('li#billing');
+        await page.waitFor(2000);
+        await browser.close();
     
-    });
-
-    afterAll( async() =>{
-        
-            await browser.close();
+     
+    })
     })
 
+    
 
 
